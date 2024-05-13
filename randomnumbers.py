7f3bed072d967abf1176ff0d75adb0ded0543039
Ivan <114067618+IvanditoRakaputra@users.noreply.github.com>
2024-05-13 23:08:25 +0700

    Update randomnumbers.py

--- randomnumbers.py
+++ randomnumbers.py
@@ -5,7 +5,6 @@ unique_numbers = set()
 while len(unique_numbers) < 1000:
     unique_numbers.add(random.randint(0, 4999))
 
-unique_numbers_list = list(unique_numbers)  # Convert set to list if needed
-#unique_numbers_list.sort()
+unique_numbers_list = list(unique_numbers)
 for i in unique_numbers_list:
     print(random.randint(0, 4999))
